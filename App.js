dfecf65f586b348a0bb03311c4a3ec32c29c3652
JoaoPedroli <joaopedrocz12345@gmail.com>
2023-09-15 18:55:50 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ResizeMode, Video, VideoFullscreenUpdate } from "expo-av";
+import * as ScreenOrientation from "expo-screen-orientation";
+import { useRef, useState } from "react";
+import { Button, Platform, StyleSheet, View } from "react-native";
 
 export default function App() {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+
+  // For Android, we need to unlock the screen to make the fullscreen feature work
+  const onFullscreenUpdate = async ({ fullscreenUpdate }) => {
+    if (Platform.OS === "android") {
+      if (fullscreenUpdate === VideoFullscreenUpdate.PLAYER_DID_PRESENT) {
+        await ScreenOrientation.unlockAsync();
+      } else if (fullscreenUpdate === VideoFullscreenUpdate.PLAYER_WILL_DISMISS) {
+        // lock the screen in Portrait orientation
+        await ScreenOrientation.lockAsync(
+          ScreenOrientation.OrientationLock.PORTRAIT
+        );
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+        onFullscreenUpdate={onFullscreenUpdate}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? "Pause" : "Play"}
+          onPress={() =>
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +51,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  video: {
+    alignSelf: "center",
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,11 @@
+## Expo AV Video Component
+
+### Notes:
+
+#### * This Expo Video Component does not offer video control customization. So, if you want this feature, you will need to implement it yourself or check out the Expo Video Player component.
+
+### * Although this Expo video component supports fullscreen, some cell phones may require disabling the 'Lock Orientation' option to watch the video with the screen in Landscape orientation.
+
+#### * The Expo Video Player Component provides customization for video controls, but it does not include fullscreen functionality. Therefore, if you want this feature, you will need to implement it yourself. Examples of fullscreen implementations using Expo Video Player: https://github.com/ihmpavel/expo-video-player/blob/master/example-app/App.tsx#L182
+
+#### * This repository will demonstrate how to implement the Expo AV Video Component to work on both Android and iOS.
